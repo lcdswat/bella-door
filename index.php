a47b97e10446ae09e0770ba02e51eb49d4f6855b
Marat Bakirov <lcdswat@debian-web>
2017-06-02 06:40:20 -0500

    upload

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 <html>
 <head>
 	<title>Bella Door</title>
+	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="/site-files/main.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 	<script src="http://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+	<link rel="shortcut icon" href="/site-files/favicon.png" type="image/png"> 
 </head>
 <body>
 
@@ -13,13 +15,13 @@
 
 <div id="header">
 	<div id="contacts">
-		8 917 123 45 78<br>
+		8 987 472-27-02<br>
 		г. Уфа, ул. Менделеева, 158
 	</div>
 	<div id="logo"></div>
 </div>
 
-<div class="grid">
+<div class="grid" id="what">
 	<h1>Межкомнатные<br>двери</h1>
 	Мы продаем межкомнатные двери из&nbsp;натурального массива дерева от&nbsp;известных производителей. Приезжайте в&nbsp;наш салон и&nbsp;подберите дверь на&nbsp;свой вкус
 </div>
@@ -35,8 +37,8 @@
 			controls: []
 		});
 		myPlacemark = new ymaps.Placemark([54.72478056997075,56.0211779999999], { 
-			hintContent: 'LABELSTAR', 
-			balloonContent: 'LABEL<b>STAR</b><br>г. Самара, ул. Ерошевского, 15, офис 9' 
+			hintContent: 'Bella Door', 
+			balloonContent: 'г. Уфа, ул. Менделеева, 158' 
 		});
 		myMap.geoObjects.add(myPlacemark);
 		myMap.behaviors
@@ -44,16 +46,16 @@
     }
 </script>
 
-
-	<div id="map-header">
-		<h1>Наш новый адрес</h1>
-		г.&nbsp;Уфа, ул.&nbsp;Менделеева,&nbsp;158<br>Выстовочный комплекс ВДНХ
-	</div>
-<div id="map">
+<!-- Карта -->
+<div id="map-header">
+	<h1>Наш новый адрес</h1>
+	г.&nbsp;Уфа, ул.&nbsp;Менделеева,&nbsp;158<br>Выстовочный комплекс ВДНХ
 </div>
 
+<div id="map"></div>
+
 <div id="footer">
-	Сайт находится в&nbsp;разработке. По всем вопросам звоните +7&nbsp;999&nbsp;133&nbsp;15&nbsp;96
+	Сайт находится в&nbsp;разработке.<br>По всем вопросам звоните <a href="tel:+79874722702">+7 987 472-27-02</a>
 </div>
 
 <!-- Прелоадер -->
